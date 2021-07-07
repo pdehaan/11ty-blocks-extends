423c4c8f70a6bc0fe168580f11d108ee0ec55621
Peter deHaan <peter@deseloper.com>
2021-07-06 17:46:10 -0700

    Remove .md support, add markdown filter+shortcode

--- .eleventy.js
+++ .eleventy.js
@@ -1,21 +1,26 @@
 const del = require("del").sync;
+const dedent = require("dedent");
 const markdownIt = require("markdown-it");
 
 module.exports = function (eleventyConfig) {
+  // Purge the `output` directory.
   del("./www");
 
   const markdownLib = markdownIt({
     html: true,
-    break: false,
     linkify: true,
+    typographer: true,
   }).disable("code");
   eleventyConfig.setLibrary("md", markdownLib);
+
+  eleventyConfig.addFilter("md", (text) => markdownLib.render(dedent(text)));
+  eleventyConfig.addPairedShortcode("md", eleventyConfig.getFilter("md"));
 
   return {
     dataTemplateEngine: "njk",
     markdownTemplateEngine: "njk",
     htmlTemplateEngine: "njk",
-    templateFormats: ["njk", "md", "html"],
+    templateFormats: ["njk", "html"],
 
     dir: {
       input: "src",
